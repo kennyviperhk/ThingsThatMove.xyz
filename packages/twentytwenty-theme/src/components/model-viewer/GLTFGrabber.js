b5100dc1f0c60eaf7a19840b97f89d8155456393
alienshek337 <alienshek337@gmail.com>
2021-06-09 18:52:17 +0800

    update (test for animation)

--- packages/twentytwenty-theme/src/components/model-viewer/GLTFGrabber.js
+++ packages/twentytwenty-theme/src/components/model-viewer/GLTFGrabber.js
@@ -2,23 +2,35 @@ import { connect, styled } from 'frontity'
 import React, { useState, useRef, Suspense } from 'react'
 import { Canvas, useFrame,  render, events, useLoader  } from '@react-three/fiber'
 import { ContactShadows, Environment, useFBX, useGLTF, useAnimations, OrbitControls } from '@react-three/drei'
+import * as THREE from 'three'
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const LoadModel = ({ data }) => {
-  //let props
-  //const group = useRef()
+  const ref = useRef()
   console.log("loading  " +data.guid)
-  //const { nodes, materials, animations } = useGLTF(data.guid)
   const gltf = useLoader(GLTFLoader, data.guid);
+
+  let mixer
+  if (gltf.animations.length) {
+      mixer = new THREE.AnimationMixer(ref.scene);
+      gltf.animations.forEach(clip => {
+          const action = mixer.clipAction(clip)
+          action.play();
+      });
+  }
+
+  useFrame((state, delta) => {
+      mixer?.update(delta)
+  })
+  // useFrame((state) => {
+  //   const t = state.clock.getElapsedTime()
+  //   ref.current.rotation.z = -0.2 - (1 + Math.sin(t / 1.5)) / 20
+  //   ref.current.rotation.x = Math.cos(t / 4) / 8
+  //   ref.current.rotation.y = Math.sin(t / 4) / 8
+  //   ref.current.position.y = (1 + Math.sin(t / 1.5)) / 10
+  // })
   return (
-    // <group ref={group} {...props} dispose={null}>
-    //   <group name="Armature" rotation={[Math.PI / 2, 0, 0]} scale={0.01} position={[0, 0, 0]}>
-    //     <primitive object={nodes.mixamorigHips} />
-    //     <skinnedMesh geometry={nodes.Alpha_Joints.geometry} material={materials.Alpha_Joints_MAT} skeleton={nodes.Alpha_Joints.skeleton} />
-    //     <skinnedMesh geometry={nodes.Alpha_Surface.geometry} material={materials.Alpha_Body_MAT} skeleton={nodes.Alpha_Surface.skeleton} />
-    //   </group>
-    // </group>
-    <primitive object={gltf.scene} scale={0.1} />
+    <primitive ref={ref} object={gltf.scene} rotation={[0,-0.3,0]} scale={0.07} />
   )
 }
 const GLTFGrabber = ({ data }) => {
@@ -26,13 +38,13 @@ const GLTFGrabber = ({ data }) => {
   return (
     <div>
     <ModelViewerSection>
-    <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 5], fov: 50 }}>
-        <ambientLight intensity={0.1} />
-        <spotLight intensity={0.1} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
+    <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 5], fov: 30 }}>
+        <ambientLight intensity={0.3} />
+        <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
         <Suspense fallback={null}>
           <LoadModel data={data} />
           <Environment preset="city" />
-          <ContactShadows rotation-x={Math.PI / 2} position={[0, -0.8, 0]} opacity={0.25} width={10} height={10} blur={1.5} far={0.8} />
+          <ContactShadows rotation-x={Math.PI / 2} position={[0, -0.8, 0]} opacity={1} width={10} height={10} blur={1.5} far={0.8} />
         </Suspense>
         <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={false} enablePan={false} />
       </Canvas>
