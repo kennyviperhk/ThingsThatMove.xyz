f376b4d71eef40625dd46d2f8003417a11889ff9
Kenny Wong Chi-Chuen <kennyviperhk@gmail.com>
2025-04-16 14:19:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-const server = require("./build/server");
+const path = require("path");
+const express = require("express");
+const frontity = require("@frontity/core");
 
-module.exports = (req, res) => {
-  const app = server.default || server;
-  return app(req, res);
-};
+const server = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the build directory
+server.use("/static", express.static(path.join(__dirname, "build/static")));
+server.use("/client", express.static(path.join(__dirname, "build/client")));
+server.use("/bundling", express.static(path.join(__dirname, "build/bundling")));
+
+// Let Frontity handle the rest
+server.all("*", async (req, res) => {
+  await frontity.serve(req, res);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
