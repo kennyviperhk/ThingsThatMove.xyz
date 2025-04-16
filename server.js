32365d76a09f1ff3b5a619448cac7db6a5ba2345
Kenny Wong Chi-Chuen <kennyviperhk@gmail.com>
2025-04-16 15:20:11 +0800

    add log from gpt

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-const server = require("./build/server");
+const express = require("express");
+const path = require("path");
+const frontity = require("@frontity/core");
 
-module.exports = (req, res) => {
-  const app = server.default || server;
-  return app(req, res);
-};
+const server = express();
+const port = process.env.PORT || 3000;
+
+// Serve static assets from build
+server.use("/static", express.static(path.join(__dirname, "build/static")));
+server.use("/client", express.static(path.join(__dirname, "build/client")));
+server.use("/bundling", express.static(path.join(__dirname, "build/bundling")));
+
+// Let Frontity handle SSR
+server.use(frontity({ mode: "production" }));
+
+server.listen(port, () => {
+  console.log(`✅ Frontity server running on http://localhost:${port}`);
+});
