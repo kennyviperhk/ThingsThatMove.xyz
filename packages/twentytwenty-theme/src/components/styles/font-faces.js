7d678222bb8d1841c1e0a06ac5a5c7134f795ca7
Kenny Wong Chi-Chuen <kennyviperhk@gmail.com>
2025-04-18 19:48:56 +0800

    try font

--- packages/twentytwenty-theme/src/components/styles/font-faces.js
+++ packages/twentytwenty-theme/src/components/styles/font-faces.js
@@ -1,57 +1,14 @@
 import { Global, css, connect } from "frontity";
-
-
-let IBMPlexSansThin = "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100&display=swap";
-
-let IBMPlexSansExtraLight = "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@200&display=swap";
-
-let IBMPlexSansLight = "https://fonts.gstatic.com/s/ibmplexsans/v8/zYX9KVElMYYaJe8bpLHnCwDKjXr8AIFsdA.woff2";
-
-let IBMPlexSansRegular = "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400&display=swap";
-
-let IBMPlexSansMedium = "https://fonts.gstatic.com/s/ibmplexsans/v8/zYX9KVElMYYaJe8bpLHnCwDKjSL9AIFsdA.woff2";
-
-let IBMPlexSansSemiBold = "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@600&display=swap";
-
-let IBMPlexSansBold = "https://fonts.gstatic.com/s/ibmplexsans/v8/zYX9KVElMYYaJe8bpLHnCwDKjWr7AIFsdA.woff2";
-
 import ArchivGroteskRegular from "../../fonts/ArchivGroteskTrial-RegularTrial.woff2";
-//let  ArchivGroteskRegularLink = "../../static/fonts/ArchivGroteskTrial-RegularTrial.woff2";
-//let ArchivGroteskRegularWP = "https://www.blog.thingsthatmove.xyz/wp-content/uploads/2022/03/ArchivGroteskTrial-RegularTrial.woff2";
-/*
-
-@font-face {
-  font-family: "Archiv Grotesk";
-  font-style: normal;
-  font-weight: 300;
-  src: url(${fonts[7]}) format("woff2");
-  font-display: ${fontDisplay};
-}
-*/
-
 
 const FontFace = ({ state }) => {
-
-  let fonts = null;
-  let fontDisplay = "swap";
-  switch (state.theme.fontSets) {
-    case "us-ascii":
-      fonts = [IBMPlexSansThin, IBMPlexSansExtraLight, IBMPlexSansLight, IBMPlexSansRegular, IBMPlexSansMedium, IBMPlexSansSemiBold, IBMPlexSansBold, ArchivGroteskRegular];
-      fontDisplay = "block";
-      break;
-    case "latin":
-      fonts = [IBMPlexSansThin, IBMPlexSansExtraLight, IBMPlexSansLight, IBMPlexSansRegular, IBMPlexSansMedium, IBMPlexSansSemiBold, IBMPlexSansBold, ArchivGroteskRegular];
-      break;
-    default:
-      fonts = [IBMPlexSansThin, IBMPlexSansExtraLight, IBMPlexSansLight, IBMPlexSansRegular, IBMPlexSansMedium, IBMPlexSansSemiBold, IBMPlexSansBold, ArchivGroteskRegular];
-  }
+  const fontDisplay = state.theme.fontSets === "us-ascii" ? "block" : "swap";
 
   return (
     <Global
       styles={css`
-
+        /* Preload the custom font */
         @font-face {
-          font-display: swap;
           font-family: "Archiv Grotesk";
           font-style: normal;
           font-weight: 300;
@@ -59,25 +16,17 @@ const FontFace = ({ state }) => {
           font-display: ${fontDisplay};
         }
 
-        @font-face {
-          font-display: swap;
-          font-family: "IBM Plex Sans";
-          font-style: normal;
-          font-weight: 500;
-          src: url(${fonts[5]}) format("woff2");
-          font-display: ${fontDisplay};
+        /* Load IBM Plex Sans from Google Fonts */
+        @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500;600&display=swap');
+
+        /* Fallback font styles */
+        body {
+          font-family: "IBM Plex Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
         }
 
-        @font-face {
-          font-display: swap;
-          font-family: "IBM Plex Sans";
-          font-style: normal;
-          font-weight: 300;
-          src: url(${fonts[3]}) format("woff2");
-          font-display: ${fontDisplay};
+        .archiv-grotesk {
+          font-family: "Archiv Grotesk", "IBM Plex Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
         }
-
-
       `}
     />
   );
